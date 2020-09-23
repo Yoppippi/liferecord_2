78447e91f020e87591e17de9f39d5b691c24282b
Yoppippi <yottiii0111@gmail.com>
2020-09-23 21:52:42 +0900

    scssファイルの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    get_week
+
     @user = User.find(params[:id])
     @meals = Meal.all 
     @motions = Motion.all
@@ -22,5 +24,27 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:nickname, :email)
+  end
+
+  def get_week
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
+    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
+    @todays_date = Date.today - 3
+    # 例) 今日が2月1日の場合・・・ Date.today.day => 1日
+
+    @week_days = []
+
+    # @plans = Plan.where(date: @todays_date..@todays_date + 7)
+
+    7.times do |x|
+      plans = []
+      # plan = @plans.map do |plan|
+        # plans.push(plan.plan) if plan.date == @todays_date + x
+      # end
+      days = { month: (@todays_date + x).month, date: (@todays_date + x ).day, plans: plans, wday: wdays[(@todays_date + x).wday]}
+      @week_days.push(days)
+    end
+
   end
 end
