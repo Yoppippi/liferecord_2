271b63cce0fe467e145cb033c2ddf32b35b8204f
Yoppippi <yottiii0111@gmail.com>
2020-10-09 13:49:05 +0900

    ビューの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_000656) do
+ActiveRecord::Schema.define(version: 2020_10_08_130630) do
 
   create_table "drinks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
