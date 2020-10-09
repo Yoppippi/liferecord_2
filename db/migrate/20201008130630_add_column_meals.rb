ff715536571a8b055f6e639d9a41c796d6fe443f
Yoppippi <yottiii0111@gmail.com>
2020-10-09 23:56:26 +0900

    コメントアウト

--- db/migrate/20201008130630_add_column_meals.rb
+++ db/migrate/20201008130630_add_column_meals.rb
@@ -1,6 +1,6 @@
-class AddColumnMeals < ActiveRecord::Migration[6.0]
-  def change
-    add_column :meals, :minute, :integer, after: :hour
-  end
-end
+# class AddColumnMeals < ActiveRecord::Migration[6.0]
+#   def change
+#     add_column :meals, :minute, :integer, after: :hour
+#   end
+# end
 
